f2e7508f7f4e928d9bf7d6a2a224c24c583452dc
deeprajiflair <deepraj.chouhan@iflair.com>
2024-04-12 18:41:51 +0530

    new one update

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+ const app =  express(); 
+ const port = 3000;
+
+ app.get('/',(req,res)=>{
+    res.send('connected successfully')
+ });
+
+ app.get('/user',(req,res)=>{
+ 
+    res.send('hiiii')
+ });
+
+ app.listen(port,() => {
+    console.log('http://localhost:',port);
+ })
