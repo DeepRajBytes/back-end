272405e9aa806478764362d4874337f891f7f91a
deeprajiflair <deepraj.chouhan@iflair.com>
2024-04-12 18:36:49 +0530

    new update

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-// const stripe = require('stripe')("sk_test_51P312RSEbzC1lRyqBXyqG1PwSGJuQdbvVs6g5mAxrTyIhHSRUoodvc8fi6NmBNkzgKsQuq8sj6QaoHwwNcYpXBhO00FGUZhxqN"); // Replace with your actual secret key
-// const mongoose = require('mongoose')
+const stripe = require('stripe')("sk_test_51P312RSEbzC1lRyqBXyqG1PwSGJuQdbvVs6g5mAxrTyIhHSRUoodvc8fi6NmBNkzgKsQuq8sj6QaoHwwNcYpXBhO00FGUZhxqN"); // Replace with your actual secret key
+const mongoose = require('mongoose')
 
 // Import middleware
 app.use(express.json());
@@ -19,49 +19,49 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// const uri="mongodb+srv://deviflair2020:admin@cluster0.bwhbeak.mongodb.net/"
+const uri="mongodb+srv://deviflair2020:admin@cluster0.bwhbeak.mongodb.net/"
 
-// mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-//     .then(() => {
-//         console.log('Connected to MongoDB');
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to MongoDB');
         
-//     })
-//     .catch(error => {
-//         console.error('Error connecting to MongoDB:', error);
-//     });
+    })
+    .catch(error => {
+        console.error('Error connecting to MongoDB:', error);
+    });
 
 
 
 // Routes import
-// const authroutes = require('./routes/auth.routes')
-// app.use('/auth', authroutes);
+const authroutes = require('./routes/auth.routes')
+app.use('/auth', authroutes);
 
-// const userroutes = require('./routes/user.routes')
-// app.use('/api/users', userroutes);
+const userroutes = require('./routes/user.routes')
+app.use('/api/users', userroutes);
 
-// const productroute = require('./routes/product.routes')
-// app.use('/api/products/',productroute);
+const productroute = require('./routes/product.routes')
+app.use('/api/products/',productroute);
 
-// const adminproduct = require('./routes/adminproduct.routes')
-// app.use('/api/admin/products/',adminproduct);
+const adminproduct = require('./routes/adminproduct.routes')
+app.use('/api/admin/products/',adminproduct);
 
-// const cartroute = require('./routes/cart.routes')
-// app.use('/api/cart/',cartroute);
+const cartroute = require('./routes/cart.routes')
+app.use('/api/cart/',cartroute);
 
-// const cartitemroute = require('./routes/cartitem.routes')
-// app.use('/api/cart_items/',cartitemroute)
+const cartitemroute = require('./routes/cartitem.routes')
+app.use('/api/cart_items/',cartitemroute)
 
-// const orderroute = require('./routes/order.routes')
-// app.use('/api/orders',orderroute);
+const orderroute = require('./routes/order.routes')
+app.use('/api/orders',orderroute);
 
-// const adminorderroute = require('./routes/adminorder.routes');
-// app.use('/api/admin/orders',adminorderroute);
+const adminorderroute = require('./routes/adminorder.routes');
+app.use('/api/admin/orders',adminorderroute);
 
-// const ratingroute = require("./routes/rating.routes");
-// app.use('/api/ratings',ratingroute);
+const ratingroute = require("./routes/rating.routes");
+app.use('/api/ratings',ratingroute);
 
-// const reviewroute = require('./routes/review.routes');
-// app.use('/api/reviews',reviewroute);
+const reviewroute = require('./routes/review.routes');
+app.use('/api/reviews',reviewroute);
 
 app.get('/', (req, res) => {
     return res.status(200).send({ message: "all is well", status: true });
